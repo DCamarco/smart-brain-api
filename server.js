76d3a126e9e1220bc199b07f1c66839043683a4e
DCamarco <doccamarco@gmail.com>
2022-12-12 11:30:23 -0500

    Server Deployment

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cors());
 
 
-app.get('/', (req, res)=> { res.send(database.users) })
+app.get('/', (req, res)=> { res.send('Server Active!') })
 
 app.post('/signin', signin.handleSignin(db, bcrypt))
 
